ba05999cbf97389584598f33b912a8cecad69ab2
Ranjeet <techformation.ranjeetthakur@gmail.com>
2020-03-10 16:41:05 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,39 @@
+const products = [];
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename),
+        'data',
+        'products.json');
+        
+const getProductsFromFile = (cb) => {
+
+        fs.readFile(p, (err, fileContent) => {
+            if(err)
+            {
+                return cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
+}
+module.exports = class Product
+{
+    constructor(t){
+        this.title = t
+    }
+
+    save()
+    {
+        // products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    static fetchAll(cb)
+    {
+        getProductsFromFile(cb);
+    }
+}
