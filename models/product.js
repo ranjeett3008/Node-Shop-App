57c59da22e053357402212321290c0252e3f2029
Ranjeet <techformation.ranjeetthakur@gmail.com>
2020-05-10 15:42:16 +0530

    Cart process  in progress

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,27 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+    
+    
     getProductsFromFile(products => {
+      if(this.id){
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProduct = [...products];
+        updatedProduct[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      
+      }
+      else
+      {
+        this.id = Math.random().toString();
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
+      }
+      
     });
   }
 
@@ -45,5 +61,13 @@ module.exports = class Product {
         const product = products.find(p=>{ return p.id==id;})
         cb(product);
       });
+  }
+
+  static deleteById(id)
+  {
+    getProductsFromFile(products => {
+      const productIndex = products.findIndex(prod=>prod.id ===id);
+      cb(product);
+    });
   }
 };
